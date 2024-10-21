e8ffddf26d1da7ff42dfcba0e93f43c16b9542de
Kendrick Mitchell <39783561+Kenmitchell2012@users.noreply.github.com>
2024-10-21 12:07:14 -0500

    type:  style
    -removing white spacing and project comments

--- js/app.js
+++ js/app.js
@@ -1,19 +1,4 @@
 /**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
  * Define Global Variables
  * 
  */
@@ -105,20 +90,9 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-// Add class 'active' to section when near top of viewport
-
-// Scroll to anchor ID using scrollTO event
-
-
 /**
  * End Main Functions
  * Begin Events
  * 
  */
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
 
